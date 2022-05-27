077be90bd1a3ff4f4cd84f4b34d71520a064e5dd
Megan Phillips <megphillips21@gmail.com>
2022-05-27 16:19:58 -0700

    mae Battleship class

---
+++ battlefield.py
@@ -0,0 +1,20 @@
+
+
+
+class Robot:
+   
+   def __init__(self): 
+    self.robot = self.robot()
+    self.dinosaur = self.dinosaur()
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
