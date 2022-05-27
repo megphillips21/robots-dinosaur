11bdc5cf99576945340327f931243b8f9d57c183
Megan Phillips <megphillips21@gmail.com>
2022-05-27 16:25:38 -0700

    added class for robots

---
+++ robot.py
@@ -0,0 +1,13 @@
+
+class Robot:
+
+    def __init__(self):
+        self.name = ''
+        self.health = ''
+        self.active_weapon = self.Weapon()
+
+    def attack(self, dinosaur):
+        pass
+
+    
+
