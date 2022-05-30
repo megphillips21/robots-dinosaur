5e3c09d7aa885bb6b46f44e4865fb7a5239f40a3
Megan Phillips <megphillips21@gmail.com>
2022-05-30 15:36:12 -0700

    run game while loop not updating the conditional so its stuck in an infinite loop, waiting for help

--- battlefield.py
+++ battlefield.py
@@ -17,28 +17,32 @@ class Battlefield:
         print("\nWelcome to the fight! Find out who is stronger, a robot or a dinosaur!\n Time to place your bets!!\n")
         pass
     
-    def battle_phase(self):
-        still_alive = True
-
-        while still_alive == True:
-            self.robot.attack(self.dinosaur)
-            self.dinosaur.attack(self.robot)
-
-            if self.robot.health <= 0 or self.dinosaur.health <=0:
-                still_alive = False
-            if self.robot.health <=0:
-                print(f'{self.dinosaur.name} has defeated {self.robot.name}')
-            elif self.dinosaur.health <=0:
-                print(f'{self.robot.name} has defeated {self.dinosaur.name}')
-            if still_alive == False:
-                self.display_winner
-            pass
-
-        
-
     def display_winner(self):
         if self.robot.health > 0:
             print(f"{self.robot.name} is the winner! {self.dinosaur} has been defeated by the robot's {self.robot.active_weapon}")
         elif self.dinosaur.health > 0:
             print(f'{self.dinosaur.name} is the winner! {self.robot.name} has been defeated! RIP {self.robot.name}')
         pass
+    
+    def battle_phase(self):
+        still_alive = True
+        while (still_alive == True):
+            self.robot.attack(self.dinosaur)
+            self.dinosaur.attack(self.robot)
+            if self.robot.health > 0:
+                still_alive == True
+            if self.dinosaur.health > 0:
+                still_alive == True
+            if self.robot.health <= 0 or self.dinosaur.health <=0:
+                still_alive == False
+            if self.robot.health <=0:
+                print(f'{self.dinosaur.name} has defeated {self.robot.name}')
+            elif self.dinosaur.health <=0:
+                print(f'{self.robot.name} has defeated {self.dinosaur.name}')
+            # if still_alive == False:
+            #     self.display_winner()
+            pass
+
+        
+
+  
