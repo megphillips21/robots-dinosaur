6f2930d4d9f43f24ae7dd136bed81827eecf3c50
Megan Phillips <megphillips21@gmail.com>
2022-05-30 14:16:30 -0700

    added attack method of robot

--- robot.py
+++ robot.py
@@ -2,13 +2,16 @@ from weapon import Weapon
 
 class Robot:
 
-    def __init__(self, name, health):
+    def __init__(self, name):
         self.name = name
-        self.health = health
+        self.health = 100
         self.active_weapon = Weapon('Laser', 20)
 
     def attack(self, dinosaur):
-        pass
+        dinosaur.health -= self.active_weapon.attack_power
+        print(f'{self.name} has attacked {dinosaur.name} with {self.active_weapon}')
+        print(f'{dinosaur.name} health is now {dinosaur.health}')
+        print(f'{self.name} health is still at {self.health}')
 
 
 
